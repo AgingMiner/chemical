b3bd4241eeffcba630792738e42925c1b0e885ae
issaku yamada <yamadaissaku@gmail.com>
2018-05-25 20:07:39 +0900

    Add files via upload
    add script

---
+++ python/pubchem-chebi.py
@@ -0,0 +1,32 @@
+import argparse
+import gzip
+# coding: utf-8
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file",
+                    help="read file")
+args = parser.parse_args()
+if args.file:
+    print("args.file is defined. file = " , args.file)
+else:
+    print("args.file is undefined.")
+
+sdf_data =  gzip.open(args.file, "rt")
+
+chebibool = False
+for line in sdf_data:
+    if "PUBCHEM_EXT_DATASOURCE_REGID" in line:
+        chebibool = True
+    if chebibool == True:
+        #chebibool = False
+        if "CHEBI:" in line:
+            print (line, end='')
+            filename = args.file
+            filename += "_result.txt"
+            f = open(filename, "a", encoding="utf-8")
+            f.write(line)
+            f.flush()
+            f.close()
+            chebibool = False
+
+sdf_data.close()
