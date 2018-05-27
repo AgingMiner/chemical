97d5d8ee3cffcc649e4106bed3611e4f8f71a9dc
issaku yamada <yamadaissaku@gmail.com>
2018-05-27 12:53:46 +0900

    Add files via upload

--- python/get_chebi-id_Synonyms.py
+++ python/get_chebi-id_Synonyms.py
@@ -3,8 +3,7 @@ import gzip
 # coding: utf-8
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file",
-                    help="read file")
+parser.add_argument("--file", help="read file")
 args = parser.parse_args()
 if args.file:
     print("args.file is defined. file = " , args.file)
@@ -17,63 +16,91 @@ chebi_id_bool = False
 chebi_name_bool = False
 chebi_iupac_bool = False
 chebi_synonyms_bool = False
+write_data = ""
 write_list = []
 filename = args.file
 filename += "_result.txt"
 f = open(filename, "a", encoding="utf-8")
+f.write('CHEBI\tprefLabel\taltLabel\n')
+f.flush()
+f.close()
+
 for line in sdf_data:
-    write_data = ""
+    
     # ChEBI ID
     if "<ChEBI ID>" in line:
         chebi_id_bool = True
     if chebi_id_bool == True:
-        #chebi_id_bool = False
-        if "CHEBI:" in line:
+        if "<ChEBI ID>" in line:
             print (line, end='')
-            cwork=line.replace("\n", "")
-            write_data += cwork + "\t"
-            chebi_id_bool = False
+        else:
+            if '\n' is line:   
+                chebi_id_bool = False
+            else:
+                #print (line, end='')
+                cwork=line.replace("\n", "")
+                write_data += cwork + "\t"
+    
     # <ChEBI Name>
     if "<ChEBI Name>" in line:
         chebi_name_bool = True
     if chebi_name_bool == True:
-        if '\n' is not line:
+        if "<ChEBI Name>" in line:
             print (line, end='')
-            nwork=line.replace("\n", "")
-            write_data += nwork + "\t"
-        if '\n' is line:   
-            chebi_name_bool = False
+        else:
+            if '\n' is line:   
+                chebi_name_bool = False
+            else:
+                #print (line, end='')
+                nwork=line.replace("\n", "")
+                write_data += nwork + "\t"
     
     # <IUPAC Names>
     if "<IUPAC Names>" in line:
         chebi_iupac_bool = True
     if chebi_iupac_bool == True:
-        if '\n' is not line:
+        if "<IUPAC Names>" in line:
             print (line, end='')
-            iwork=line.replace("\n", "")
-            write_list.append(iwork)
-        if '\n' is line:   
-            chebi_iupac_bool = False
+        else:
+            if '\n' is line:   
+                chebi_iupac_bool = False
+            else:
+                #print (line, end='')
+                iwork=line.replace("\n", "")
+                write_list.append(iwork)
 
     # <Synonyms>
     if "<Synonyms>" in line:
         chebi_synonyms_bool = True
     if chebi_synonyms_bool == True:
-        if '\n' is not line:
+        if "<Synonyms>" in line:
             print (line, end='')
-            swork=line.replace("\n", "")
-            write_list.append(swork)
-        if '\n' is line:   
-            chebi_synonyms_bool = False
+        else:
+            if '\n' is line:   
+                chebi_synonyms_bool = False
+            else:
+                #print (line, end='')
+                swork=line.replace("\n", "")
+                write_list.append(swork)
 
+# $$$$
     if "$$$$" in line:
-        if len(write_list) is not 0:
-            for name in write_list:
-                f.write(write_data + name + "\n")
-                f.flush()
+        f = open(filename, "a", encoding="utf-8")
         if len(write_list) is 0:
-            f.write(write_data + "\n")
-            f.flush()
+            linedata = write_data + "\n"
+            f.write(linedata)
+            print (linedata, end='')
+            write_data = ""
+            write_list = []
+        else:
+            for name in write_list:
+                linedata = write_data + name + "\n"
+                f.write(linedata)
+                print (linedata, end='')
+            write_data = ""
+            write_list = []
+        
+        f.flush()
+        f.close()
 
-f.close()
 sdf_data.close()
